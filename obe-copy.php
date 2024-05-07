968a1da4d30dd73da5cd30292f9c751b7a4c71b4
violetto-rose <manjumadhav.va@gmail.com>
2024-05-07 23:45:29 +0530

    modified:   obe-copy.css 	modified:   obe-copy.php

--- obe-copy.php
+++ obe-copy.php
@@ -26,86 +26,86 @@
 <body>
   <?php
 
-// Function to add suffix to semester numbers
-function addSuffix($semester)
-{
-  switch ($semester) {
-    case 3:
-      return $semester . 'rd';
-    case 4:
-      return $semester . 'th';
-    case 5:
-      return $semester . 'th';
-    case 6:
-      return $semester . 'th';
-    case 7:
-      return $semester . 'th';
-    case 8:
-      return $semester . 'th';
+  // Function to add suffix to semester numbers
+  function addSuffix($semester)
+  {
+    switch ($semester) {
+      case 3:
+        return $semester . 'rd';
+      case 4:
+        return $semester . 'th';
+      case 5:
+        return $semester . 'th';
+      case 6:
+        return $semester . 'th';
+      case 7:
+        return $semester . 'th';
+      case 8:
+        return $semester . 'th';
+    }
   }
-}
 
-// Establish a database connection
-$servername = "localhost";
-$username = "root";
-$dbname = "csd";
+  // Establish a database connection
+  $servername = "localhost";
+  $username = "root";
+  $dbname = "csd";
 
-$conn = new mysqli($servername, $username, "", $dbname);
+  $conn = new mysqli($servername, $username, "", $dbname);
 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
 
-// Fetch data from the database based on the selected scheme and semester
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $scheme = $_POST["scheme"];
-  $semester = $_POST["semester"];
-  $sql = "SELECT * FROM csd WHERE scheme_year = $scheme AND semester = $semester";
+  // Fetch data from the database based on the selected scheme and semester
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $scheme = $_POST["scheme"];
+    $semester = $_POST["semester"];
+    $sql = "SELECT * FROM csd WHERE scheme_year = $scheme AND semester = $semester";
 
-  echo "<div class='head'>
+    echo "<div class='head'>
             <h1>Department of Computer Science and Design</h1>
             <h2>OBE Report of " . addSuffix($semester) . " Semester, $scheme Scheme</h2>
             <br />
             <hr />
           </div>";
 
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-    echo "<table>
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+      echo "<table>
               <tr>
                 <th>Serial Number</th>
                 <th>Subject Number</th>
                 <th>Subject Code</th>
                 <th>Subject Name</th>
                 <th>Faculty</th>
-                <th>CO-PO Attainment</th>
               </tr>";
 
-    while ($row = $result->fetch_assoc()) {
-      $attainment = $row["attainment"];
-      $FilePath = 'attainments/' . $attainment;
+      while ($row = $result->fetch_assoc()) {
 
-      echo "<div class='outcomes'>
+        echo "<div class='outcomes'>
                 <tr>
                   <td>" . $row["serial_number"] . "</td>
                   <td>" . $row["subject_number"] . "</td>
                   <td>" . $row["subject_code"] . "</td>
                   <td>" . $row["subject_name"] . "</td>
                   <td>" . $row["faculty"] . "</td>
-                  <td><a href='$FilePath'>$FilePath</a></td>
                 </tr>
               </div>";
-    }
-    echo "</table>";
-  } else {
-    echo "<div class='result'>
+      }
+      echo "</table>";
+    } else {
+      echo "<div class='result'>
               <p>NO RESULTS</p>
             </div>";
+    }
   }
-}
-$conn->close();
+  $conn->close();
   ?>
-
+  <div class='button'>
+    <span class="btn">
+      <a href='attainment.xlxs' target="_blank">CO-PO Attainment</a>
+</span>
+  </div>
   <div class="button-container">
     <a href="main-copy.html" class="home-button">Home</a>
   </div>
